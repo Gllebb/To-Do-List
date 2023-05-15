1a4d5a9d6bd59fd6da1a664e2940771be9d6a7c4
Gllebb <myggithub@gmail.com>
2023-05-15 16:26:10 +0300

    Add files via upload

---
+++ stuff/footer.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="footer-items">
+        <ul>
+            <li>Source code:</li>
+            <li><a href="https://github.com/Gllebb/To-Do-List"><i class="fab fa-github fa-xl"></i></a></li>
+            <li>Send us Email:</li>
+            <li><a href="mailto:dimitrijevgleb@gmail.com"><i class="fas fa-envelope fa-xl"></i></a></li>
+        </ul>
+    </div>
+    
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <!-- JavaScript -->
+    <script src="stuff/index.js"></script>
+</footer>
---
+++ stuff/nav-bar.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="logo">
+        <a class="no-style" href="index.php">Things to do!</a>
+    </div>
+    <div class="nav-items">
+        <li id="current-date">Date</li>
+        <li id="current-time">Time</li>
+        <li class="profile"><a href="profile.php">Profile</a></li>
+        <li><a href="logout.php">Log Out</a></li>
+    </div>
+</nav>
