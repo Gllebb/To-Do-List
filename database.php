c77e784231250f0c1b047e6e348b609ed6aadb5d
Gllebb <myggithub@gmail.com>
2023-05-15 16:22:59 +0300

    Update database.php

--- database.php
+++ database.php
@@ -3,10 +3,9 @@
     $dbUser = "root";
     $dbPassword = "";
     $dbName = "login_register";
-    $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
+    $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName, 3306);
 
     if (!$conn){
         die("Could not connect to database");
     }
-
 ?>
