2636c5c5e5e96e03143654c3791e9c8abfe26306
Gllebb <myggithub@gmail.com>
2023-05-29 20:05:20 +0300

    Update login.php

--- login.php
+++ login.php
@@ -17,30 +17,29 @@
 </head>
 <body>
     <div class="container">
-    <?php
+        <?php
 
-    if(isset($_POST['login'])){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        require_once "database.php";
-        $sql = "SELECT * FROM users WHERE email = '$email'";
-        $result = mysqli_query($conn, $sql);
-        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        if($user){
-            if(password_verify($password, $user["password"])){
-                session_start();
-                $_SESSION["user"] = "yes";
-                header("Location: index.php");
-                die();
+        if(isset($_POST['login'])){
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if($user){
+                if(password_verify($password, $user["password"])){
+                    session_start();
+                    $_SESSION["user"] = $user["id"];
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
             }else{
-                echo "<div class='alert alert-danger'>Password does not match</div>";
+                echo "<div class='alert alert-danger'>Emails does not match</div>";
             }
-        }else{
-            echo "<div class='alert alert-danger'>Emails does not match</div>";
         }
-    }
-
-    ?>
+        ?>
         <form action="login.php" method="post">
             <div class="form-group">
                 <input type="email" class="form-control" name="email" placeholder="Enter Email:">
@@ -53,7 +52,7 @@
             </div>
         </form>
         <div>
-            <p>Not registred yet? <a href="registration.php">Register there!</a></p>
+            <p>Not registred yet? <a href="registration.php">Register here!</a></p>
         </div>
     </div>
 </body>
